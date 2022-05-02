ab9bdc3cbb98a3f83d1b3b82e746a504483b8c0c
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-05-02 16:08:04 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Logout } from "./components/Logout";
+import { NewOrder } from "./components/NewOrder";
+import { Orders } from "./components/Orders";
+// import { ProtectedRoute } from "./components/ProtextedRoute";
+import {Routes,Route,Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Link className="nav-home" to="/">
+          Home
+        </Link>
+        {/* Show either login or logout below */}
+        <Link className="nav-logout" to="/logout">
+          Logout
+        </Link>
+        <Link className="nav-login" to="/login">
+          Login
+        </Link>
+      </div>
+
+      <Routes>
+        {/* Routes are as follows:
+        Route      Component
+        /           Home
+        /login      Login
+        /logout     Logout
+        /orders     Orders    Protected
+        /neworder   NewOrder  Protected
+        */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Logout" element={<Logout />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="/neworder" element={<NewOrder />}></Route>
+      </Routes>
     </div>
   );
 }
